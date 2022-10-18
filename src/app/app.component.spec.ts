7e3120d3ac89d6b1bdb92418c56dae70e5b4c7f4
Walter Gustavo Rojo <wallywgr@gmail.com>
2022-10-18 16:59:41 -0300

    components
    Agrego los componentes : acerca-de, banner,header,logo-ap,social

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PI_RojoWalterG'`, () => {
+  it(`should have as title 'ProyectoIntegrador_RojoWalterGustavo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PI_RojoWalterG');
+    expect(app.title).toEqual('ProyectoIntegrador_RojoWalterGustavo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PI_RojoWalterG app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ProyectoIntegrador_RojoWalterGustavo app is running!');
   });
 });
